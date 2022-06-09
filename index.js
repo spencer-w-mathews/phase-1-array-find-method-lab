4b065bcee2baccb651736b7a6a6e9baed345111c
Spencer Mathews <smathews0906@gmail.com>
2022-06-09 08:26:57 -0500

    commit tree b36713971e366bd00008592ce29f0945798c9081

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  //...
+];
+
+//write a function with one array as the argument
+// Each object has its own two properties
+// us find() to test if the result=W
+//return year when the win occured if at all-- if not undefined
+
+function superbowlWin(array) {
+
+  let x = array.find(obj => {
+      return obj.result === "W"
+  })
+
+  if(x){
+      return x.year
+  }
+}
+
+//
+// record.find(superbowlWin)
+
